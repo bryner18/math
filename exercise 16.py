f898b895bc0a150ee528de0dbde1d2da4e9b2c14
Bryner <79683205+bryner18@users.noreply.github.com>
2021-03-02 20:14:01 -0800

    Update exercise 16.py

--- exercise 16.py
+++ exercise 16.py
@@ -1,6 +1,3 @@
-# 16. Promediar los x primeros múltiplos de 2 y determinar si ese promedio es mayor que los y primeros múltiplos de 5 para valores de x y y leídos.
-
-
 def main():
     equation()
 
