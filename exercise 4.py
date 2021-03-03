e21fddb596cab1ebcb7f5639592e892dbb9cf03d
Bryner <79683205+bryner18@users.noreply.github.com>
2021-03-03 06:10:50 -0800

    Update exercise 4.py

--- exercise 4.py
+++ exercise 4.py
@@ -9,16 +9,13 @@ def main():
 def two_numbers():
     n1 = int(input("Insert a number: "))
     n2 = int(input("Insert another number: "))
-    n3 = 0
-    n4 = n1 - n2
-    if n3 < n4:
-        while n3 < n4:
-            n3 = n3 + 1
-            print(n3)
-    if n3 > n4:
-        while n3 > n4:
-            n3 = n3 - 1
-            print(abs(n3))
+    
+    if n1 < n2:
+        for x in range(n1, n2 + 1):
+            print(x)
+    if n1 > n2:
+        for a in range(n1, n2, -1):
+            print(a)
 
 
 def try_again():
